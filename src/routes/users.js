e92faea5fb8b0311f2b10060800eded87842dab0
Miguel Angel Cornielly H <mcornielly@gmail.com>
2021-02-10 00:03:09 -0400

    the user registration module is complete

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/signin', (req, res) => {
+router.get('/users/signin', (req, res) => {
     // res.send('Ingresando a la app');
     res.render('users/signin');
 });
 
-router.get('/signup', (req, res) => { 
+router.get('/users/signup', (req, res) => { 
     // res.send('Formulario de autenticación');
     res.render('users/signup');
+});
+
+router.post('/users/signup', async (req, res) => {
+    const { name, email, password, confirm_password } = req.body;
+    const errors = [];
+
+    if(name.length <= 0){
+        errors.push({text: 'Debe Ingresar un Nombre.'});
+    }
+    
+    if(password != confirm_password){
+        errors.push({text: 'Password no coincide'});
+    }
+
+    if(password.length < 4) {
+        errors.push({text: 'Password debe ser mayor a 4 caracteres'});
+    }
+
+    if(errors.length > 0) {
+        res.render('users/signup', {errors, name, email, password, confirm_password});
+    }else{
+        const emailUser = await User.findOne({ email: email });
+        if(emailUser){
+            reg.flash('error_msg', 'El Email esta en uso');
+        }
+        const newUser = new User({name, email, password});
+        newUser.password = await newUser.encryptPassword(password);
+        await newUser.save();
+        req.flash('success_msg', 'El Usuario ha sido registrado');
+        res.redirect('/users/signin');
+        // res.send("Ok");
+    }
+    // console.log(req.body);
 });
 
 module.exports = router;
